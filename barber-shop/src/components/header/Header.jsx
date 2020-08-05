89033a94d7dd2e096f219ae247781588b8116d05
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-08-05 14:56:42 -0300

    iniciando projeto e configurando css reset

---
+++ barber-shop/src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './header.css'
+
+export default function Header() {
+    return (
+        <header className='header'>
+            <h1>asdasdasd</h1>
+            <h1>asdfd6df54d65f4d6</h1>
+        </header>
+    )
+}
