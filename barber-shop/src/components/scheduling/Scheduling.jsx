59e2276bf5eb99661eaa0eb4aba8663adead65e7
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-08-11 18:07:17 -0300

    formulário de agendamento

--- barber-shop/src/components/scheduling/Scheduling.jsx
+++ barber-shop/src/components/scheduling/Scheduling.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import "./scheduling.css"
 
 export default function Scheduling() {
     return (
         <>
+        <a href="/" className='linkBackHome'> {'< Home'} </a>
+        <form className='formAgend' action="" method="post">
         <h1>Agendamento</h1>
-        <form action="" method="post">
             <label htmlFor="">Name</label>
             <input type="text" placeholder='name'/>
 
@@ -12,7 +14,9 @@ export default function Scheduling() {
             <input type="date" name="" id=""/>
 
             <label htmlFor="">Hour</label>
-            <input type="datetime" name="" id=""/>
+            <input type="time" name="" id=""/>
+
+            <button type='submit'> SUBMIT </button>
         </form>
         </>
     )
